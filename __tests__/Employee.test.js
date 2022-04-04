c3a2c20c3afcb18f2bd0557b83ccad2f578b5aaf
Carl Bush <CarlBushs13@gmail.com>
2022-04-04 08:24:13 -0700

    employee class and test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,34 @@
+const Employee = require("../lib/Employee");
+
+test("creates employee and verify name,id,email" , () => {
+    const employee = new Employee("Meowzers", 5, "Meowmix@catnip.com")
+
+    expect(employee.name).toBe("Meowzers");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    
+});
+
+test("getName() | return Employee's Name", () => {
+    const employee = new Employee("Meowzers", 5, "Meowmix@catnip.com")
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test("getId() | returns Employee's ID", () => {
+    const employee = new Employee("Meowzers", 5, "Meowmix@catnip.com")
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test("getEmail() | returns Employee's email", () => {
+    const employee = new Employee("Meowzers", 5, "Meowmix@catnip.com")
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test("getRole() | returns Employee's role", () => {
+    const employee = new Employee("Meowzers", 5, "Meowmix@catnip.com")
+
+    expect(employee.getRole()).toBe("Employee")
+});
